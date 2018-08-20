5f4416727ce3fb63b5996a4d395cf379967a8f7a
Anna de Araujo <annadearaujo4@outlook.com>
2018-08-20 11:13:05 -0300

    teste
    test

--- sair.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	session_start();
-	/*função unset elimina a sessão informada como parâmetro.*/
-	unset($_SESSION['usuario']);
-	unset($_SESSION['email']);
-
-	header('Location: index.php');
- ?>
