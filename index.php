462719a07897bc2433bd516a662757170e67eb71
evelyntipi <40428639+evelyntipi@users.noreply.github.com>
2018-08-22 11:31:43 -0300

    Modificações das paginas
    Aqui foi modificado o navbar e seu midia caries

--- index.php
+++ index.php
@@ -12,41 +12,23 @@
 
 		<!-- jquery - link cdn -->
 		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-
-		<!-- bootstrap - link cdn -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<link rel="stylesheet" type="text/css" href="css/home.css">
+		<!--link css da pagina-->
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<!--link css da barra de navegação-->
+		<link rel="stylesheet" type="text/css" href="css/navbar.css">
 	
 	</head>
 <body>
-	<!--Static navbar-->
-	        
-	          <ul>
-	          	<li class="li_beta1"><span><a href="perfil.php">Meu perfil</a></span></li>
-	            <li><span><a href="entrar.php">Entrar</a></span></li>
-	            <li class="li-beta"><span class="beta1"><a href="#">News</a></span></li>
-	            <li><span class="beta2"><a href="#">Quadrinhos</a></span></li>
-	            <li><span class="beta3"><a href="#">Manga</a></span></li>
-	            <li><span class="beta4"><a href="#">Otaku Store</a></span></li>
-	          </ul>
 
-	    <div class="">
-	    	<br><br><br><br>
-	    		<div class="">
-	    			BetaComics<br><br><br>
-	    			BetaMangá<br><br><br>
-	    			BetaStore<br>
-	    		</div>
-	    		<div class="">
-	    			Destaques da semana
-	    		</div>
-	    		<div class="	">
-	    			Propagandas
-	    		</div>
-	    	
-	    </div>
-	    
+	<!--Barra de navegação. Padrão para todas as abas-->
+	<ul class="ul_navbar">
+	    <li><span><a href="#" class="link_nav">inscrever-se</a></span></li>
+	    <li><span><a href="#" class="link_nav">Entrar</a></span></li>
+	    <li><span><a href="#" class="link_nav">News</a></span></li>
+	    <li><span><a href="#" class="link_nav">Quadrinhos</a></span></li>
+	    <li><span><a href="#" class="link_nav">Manga</a></span></li>
+	    <li><span><a href="#" class="link_nav">Otaku Store</a></span></li>
+	</ul>   
 
 	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </body>
